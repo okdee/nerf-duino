f532e350990b04832c20342b6bd9833ae477659e
Omar Gharra <omar.gharrah@gmail.com>
2020-05-28 17:56:14 +0300

    Added
    Added RemoteControlNerf project directory

---
+++ RemoteControlNerf/RemoteControlNerf.ino
@@ -0,0 +1,50 @@
+#include <IRremote.h>
+#include <Servo.h>
+
+Servo triggerServo;
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+const int triggerServoPin = 8;
+const int motorRelayPin = 10;
+
+boolean fireTrigger = false;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+  
+  triggerServo.attach(triggerServoPin);
+  pinMode(motorRelayPin, OUTPUT);
+
+  digitalWrite(motorRelayPin, LOW);
+  triggerServo.write(0);
+}
+
+void loop(){
+  
+    if (irrecv.decode(&results)){
+        switch(results.value){
+          case 0xFFC23D: //Keypad button >||
+            fireTrigger = !fireTrigger;
+            if (!fireTrigger){
+              Serial.println("stop");
+              triggerServo.write(0);
+              delay(500);
+              digitalWrite(motorRelayPin, LOW);
+            }
+            else{
+              Serial.println("fire");
+              digitalWrite(motorRelayPin, HIGH);
+              delay(500);
+              triggerServo.write(70);
+            }
+            break;
+          }
+        delay(100);
+        irrecv.resume();
+    }
+}
